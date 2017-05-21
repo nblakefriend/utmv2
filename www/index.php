5ba43e39d15e3d621a264551419d035573139ec2
Nathan <nfriend@rsglab.com>
2017-05-21 09:24:36 -0400

    Added sub pages, 404 page, fixed htaccess file.

--- www/index.php
+++ www/index.php
@@ -6,24 +6,44 @@ $app = new \Slim\App;
 $container = $app->getContainer();
 
 // Twig as template engine
-$container['view'] = function ($container)
+$container['view'] = function ($c)
 {
     $view = new \Slim\Views\Twig('../app/views', [
         'cache' => false
     ]);
     $view->addExtension(new \Slim\Views\TwigExtension(
-        $container['router'],
-        $container['request']->getUri()
+        $c['router'],
+        $c['request']->getUri()
     ));
+
+    return $view;
+};
+
+//Override the default Not Found Handler
+$container['notFoundHandler'] = function ($c)
+{
+    return function ($request, $response) use ($c) {
+        return $c['view']->render($response, '404.twig', [
+        ]);
+    };
 };
 
 $app->get("/", function ($request, $response, $args)
 {
-    return $this->view->render($response, 'main.twig', [
-        
-    ]);
+    return $this->view->render($response, 'home.twig', []);
 
 })->setName('home');
+
+$app->get("/contact", function ($request, $response, $args)
+{
+    return $this->view->render($response, 'contact.twig', []);
+
+})->setName('contact');
+
+$app->get("/faq" , function ($request, $response, $args)
+{
+    return $this->view->render($response, 'faq.twig', []);
+})->setName('faq');
 
 $app->run();
 
